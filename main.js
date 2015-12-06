101a2a633eb68996f608a23ca2a224d0b9787330
chasingdonis <cuixianning@hotmail.com>
2015-12-06 10:36:54 -0600

    insert img

--- main.js
+++ main.js
@@ -20,6 +20,10 @@ $("#cui").hover(function(){
 $( "h4" ).hover(function() {
   $( this ).addClass( "ani" );
 });
+
+$( "h3" ).click(function() {
+  $( this ).slideUp();
+});
     
 });
 
@@ -37,7 +41,6 @@ function check_if_in_view() {
     var element_top_position = $element.offset().top;
     var element_bottom_position = (element_top_position + element_height);
 
-    //check to see if this current container is within viewport
     if ((element_bottom_position >= window_top_position) &&
       (element_top_position <= window_bottom_position)) {
       $element.addClass('in-view');
